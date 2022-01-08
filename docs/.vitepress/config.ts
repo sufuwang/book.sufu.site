d8528012072905e3460b97dfc91ec191cdd273e9
sufuwang <sufuwang@tencent.com>
2022-01-08 16:33:12 +0800

    feat: base

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -27,6 +27,7 @@ interface TypeConfig {
 
 // https://github.com/vuejs/vitepress/blob/main/src/node/config.ts#L15
 const config: TypeConfig = {
+  base: 'book',
   themeConfig: {
     sidebar,
   },
